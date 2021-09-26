c3e4a81bd8ecd1f4c509d59fe54d8cd78754ea00
Rumon <crash.msr@gmail.com>
2021-09-26 21:30:50 +0600

    create 3 dummy microservices

---
+++ service-2/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: { url: 'nats://nats-service.default:4222', queue: 'service-2' },
+    },
+    { inheritAppConfig: true },
+  );
+
+  await app.startAllMicroservices();
+  await app.listen(5000);
+}
+bootstrap();
